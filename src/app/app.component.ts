5432b249f9f359d4467f32a72b78d183146c0b26
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2023-08-03 09:19:10 +0700

    Revert "Add example table"
    This reverts commit e86e2ad3629331b6f5aa140c127affb2b6bd08a5.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
 import { Component } from '@angular/core';
 
-interface Product {
-  code: number;
-  name: string;
-  category: string;
-  quantity: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'finance';
-  products: Product[] = [
-    {code: 1321, name: "IPhone 14", category: "phone", quantity: 15},
-    {code: 2349, name: "Dell XPS", category: "laptop", quantity: 8},
-    {code: 2394, name: "Airpod", category: "headphone", quantity: 32},
-    {code: 3948, name: "Samsung Galaxy Note 23", category: "phone", quantity: 17},
-    {code: 4820, name: "Lenovo Legion", category: "laptop", quantity: 10},
-  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TableModule } from 'primeng/table';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, TableModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
