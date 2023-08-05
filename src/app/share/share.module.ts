946ac7729bb9b800dcda99cc5e73d306726e61ad
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2023-08-05 18:27:43 +0700

    Clean up

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DataViewComponent } from './data-view/data-view.component';
-import { PickListComponent } from './pick-list/pick-list.component';
-
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { DataViewModule } from 'primeng/dataview';
 import { RatingModule } from 'primeng/rating';
 import { TagModule } from 'primeng/tag';
@@ -12,23 +11,23 @@ import { PickListModule } from 'primeng/picklist';
 import { GalleriaModule } from 'primeng/galleria';
 import { TableModule } from 'primeng/table';
 import { OrderListModule } from 'primeng/orderlist';
-import { GalleriaComponent } from './galleria/galleria.component';
-import { TableComponent } from './table/table.component';
-import { UploadComponent } from './upload/upload.component';
-
-import { HttpClientModule } from '@angular/common/http';
-import { FileUploadModule } from 'primeng/fileupload';
 import { ToastModule } from 'primeng/toast';
-import { OrgtreeComponent } from './orgtree/orgtree.component';
 import { OrganizationChartModule } from 'primeng/organizationchart';
 import { AccordionModule } from 'primeng/accordion';
 import { TreeTableModule } from 'primeng/treetable';
-import { MenuComponent } from './menu/menu.component';
 import { TabMenuModule } from 'primeng/tabmenu';
-import { OrderListComponent } from './order-list/order-list.component';
-import { AccordionComponent } from './accordion/accordion.component';
 import { DividerModule } from 'primeng/divider';
 
+import { DataViewComponent } from './data-view/data-view.component';
+import { PickListComponent } from './pick-list/pick-list.component';
+import { FileUploadModule } from 'primeng/fileupload';
+import { GalleriaComponent } from './galleria/galleria.component';
+import { TableComponent } from './table/table.component';
+import { UploadComponent } from './upload/upload.component';
+import { OrgtreeComponent } from './orgtree/orgtree.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { AccordionComponent } from './accordion/accordion.component';
 
 @NgModule({
   declarations: [
